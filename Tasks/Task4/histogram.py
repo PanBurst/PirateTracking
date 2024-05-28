fcfac0439817e8e29eca9e5e0dac6f11bff9c206
Robert Mackevič <robert@aai-labs.com>
2024-05-28 22:08:30 +0300

    Refactored task4 code.

--- Tasks/Task4/histogram.py
+++ Tasks/Task4/histogram.py
@@ -1,65 +1,70 @@
+import random
+from datetime import datetime
+
+import matplotlib.pyplot as plt
 from pymongo import MongoClient
 from pymongo.collection import Collection
-from datetime import datetime
-import matplotlib.pyplot as plt
-import random
 from tqdm import tqdm
 
 TIME_STAMP_LIMIT = 100
 
+
 def sample_mmsi_timestamp_ranges(collection: Collection) -> list[tuple[int, int]]:
+    # Define an aggregation pipeline to group documents by the MMSI field
+    # and count the number of documents for each MMSI.
     group_by_mmsi_query = [
         {
-            '$group': {
-                '_id': '$MMSI',
-                'count': {'$sum': 1}
+            "$group": {
+                "_id": "$MMSI",
+                "count": {"$sum": 1}
             }
         }
     ]
 
-    mmsi_ranges = []
-    mmsi_groups = list(collection.aggregate(group_by_mmsi_query))
-
-    for mmsi_group in mmsi_groups:
-        upper_mmsi_range_limit = mmsi_group['count'] - TIME_STAMP_LIMIT
-        timestamp_skip = random.choice(range(0, upper_mmsi_range_limit))
-        mmsi_ranges.append((mmsi_group["_id"], timestamp_skip))
-
-    # mmsi_ranges = random.choices(mmsi_ranges, k=1000)
-    return mmsi_ranges
+    # Execute the aggregation query on the collection and iterate over the results.
+    # For each group (each unique MMSI), create a tuple consisting of the MMSI and
+    # a random starting index for sampling timestamps, ensuring the range is within
+    # the bounds of available documents (count - TIME_STAMP_LIMIT).
+    return [
+        (mmsi_group["_id"], random.choice(range(0, mmsi_group["count"] - TIME_STAMP_LIMIT)))
+        for mmsi_group in collection.aggregate(group_by_mmsi_query)
+    ]
 
 
-def to_datetime(date_str):
-    return datetime.strptime(date_str, '%d/%m/%Y %H:%M:%S')
-
-def calculate_sample_time_deltas(collection: Collection, mmsi_ranges: list[tuple[int, int]]) -> list[int]:
+def calculate_sample_time_deltas(collection: Collection, mmsi_ranges: list[tuple[int, int]]) -> list[float]:
     delta_t = []
-    for (mmsi, timestamp_skip) in tqdm(mmsi_ranges):
-        sample = list(collection.find({"MMSI": mmsi}, {"# Timestamp": 1}) \
-                                        .sort("# Timestamp", 1) \
-                                        .skip(timestamp_skip).limit(TIME_STAMP_LIMIT))
-        
-        last_time_record = to_datetime(sample[0]["# Timestamp"])
-        for record in sample[1:]:
-            current_time_record = to_datetime(record["# Timestamp"])
-            time_delta_seconds = (current_time_record - last_time_record).total_seconds()
-            delta_t.append(time_delta_seconds)
-            last_time_record = current_time_record
-    
+    for mmsi, timestamp_skip in tqdm(mmsi_ranges):
+        # Query the collection for documents with the specified MMSI,
+        # skipping the specified number of timestamps, and limiting
+        # the number of documents retrieved.
+        sample = list(collection.find(
+            {"MMSI": mmsi}, {"# Timestamp": 1}
+        ).sort("# Timestamp", 1).skip(timestamp_skip).limit(TIME_STAMP_LIMIT))
+
+        # Extract timestamps from the sample and convert them to datetime objects
+        timestamps = [datetime.strptime(record["# Timestamp"], "%d/%m/%Y %H:%M:%S") for record in sample]
+
+        # Calculate time deltas between consecutive timestamps and store them the list
+        delta_t.extend(
+            (timestamps[i] - timestamps[i - 1]).total_seconds()
+            for i in range(1, len(timestamps))
+        )
+
     return delta_t
 
 
-def plot_histogram(delta_t: list[int]):
+def plot_histogram(delta_t: list[float]):
     plt.figure(figsize=(10, 6))
-    plt.hist(delta_t, bins=1000, edgecolor='black')
-    plt.title('Histogram of Time Differences (delta_t)')
-    plt.xlabel('Time Difference (seconds)')
-    plt.ylabel('Frequency')
+    plt.hist(delta_t, bins=1000, edgecolor="black")
+    plt.title("Histogram of Time Differences (delta_t)")
+    plt.xlabel("Time Difference (seconds)")
+    plt.ylabel("Frequency")
     plt.grid(True)
+    plt.savefig("histogram.png")
     plt.show()
 
 
-if __name__ == "__main__":
+def run() -> None:
     client = MongoClient("mongodb://localhost:27141")
     db = client["sea"]
     filtered_vessels = db["filtered_vessels"]
@@ -70,3 +75,7 @@ if __name__ == "__main__":
     delta_t = calculate_sample_time_deltas(filtered_vessels, mmsi_ranges)
     plot_histogram(delta_t)
     client.close()
+
+
+if __name__ == "__main__":
+    run()
